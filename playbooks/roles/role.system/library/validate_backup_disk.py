28b8d88cac4b41dbf53df2e2f7ffcb4d4aebf516
Ansible user <ersdem@openstack>
2018-06-02 13:40:00 +0000

    first commit - adding config and playbooks, removing elma.txt

---
+++ playbooks/roles/role.system/library/validate_backup_disk.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import re
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            configured_disk=dict(type='str', required=True),
+        ),
+        # Check mode don't differ from a "real" run.
+        supports_check_mode=True
+    )
+
+    disk = module.params['configured_disk']
+
+    if disk:
+        pattern = re.compile("\/dev\/[s,v]d[a-z]")
+        if not pattern.match(disk):
+            module.fail_json(msg=str(disk) + " is not a valid disk")
+        module.exit_json(changed=False)
+    else:
+        module.fail_json(msg="The parameter backup_disk has no value defined")
+
+from ansible.module_utils.basic import *
+main()
